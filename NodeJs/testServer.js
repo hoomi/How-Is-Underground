c0de1c273a7fe86f59ed140e601dc695fc0f3ce0
Hooman Ostovari <hoomi2010@gmail.com>
2013-05-30 21:17:00 +0100

    added different responses to the test server

--- NodeJs/testServer.js
+++ NodeJs/testServer.js
@@ -2,10 +2,19 @@ var http = require('http'),
   fs = require('fs');
 
 http.createServer(function(req, res) {
-  console.log("Request from: " + req.headers.referrer);
-  respondOK(res);
-}).listen(1338, 'localhost');
-console.log('Server running at http://localhost:1338/');
+  var url = req.url;
+  console.log(req.url);
+  if (url === '/200') {
+    respondOK(res);
+  } else if (url === '/403') {
+    respondForbidden(res);
+  } else if (url === '/500') {
+    respondInternalSError(res);
+  } else {
+    respondNotFound(res);
+  }
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
 
 var respondOK = function(res) {
   var fileStats = fs.statSync('./respondOK.xml');
@@ -27,4 +36,19 @@ var respondOK = function(res) {
     res.end();
     console.log('200 OK was send successfully');
   });
+};
+
+var respondForbidden = function (res) {
+  res.writeHead(403, {'Content-Type': 'text/plain'});
+  res.end('Access to this website is forbidden\n');
+};
+
+var respondNotFound = function (res) {
+  res.writeHead(404, {'Content-Type': 'text/plain'});
+  res.end('Could not find this website\n');
+};
+
+var respondInternalSError= function (res) {
+  res.writeHead(500, {'Content-Type': 'text/plain'});
+  res.end('An internal server error\n');
 };
